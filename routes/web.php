de5d2be43a7da14ff0e4cc6155e50c59a6f59d78
MioT-camp <taitaihanahana@gmail.com>
2025-09-09 21:53:54 +0900

    新規問い合わせ登録画面

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::middleware(['auth'])->group(function () {
 
     // 問い合わせ管理
     Volt::route('inquiries', 'inquiries.index')->name('inquiries.index');
+    Volt::route('inquiries/create', 'inquiries.create')->name('inquiries.create');
     Volt::route('inquiries/{inquiry_id}', 'inquiries.show')->name('inquiries.show');
 
     // カテゴリ管理
